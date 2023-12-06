15bd550bfa87c2817f29d6d672c9fbe0a6db5a0f
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-12-06 16:02:29 +0100

    7. Java-based Configuration per profile

---
+++ rental-store/src/main/java/com/pivovarit/domain/rental/RentalConfiguration.java
@@ -0,0 +1,7 @@
+package com.pivovarit.domain.rental;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class RentalConfiguration {
+}
---
+++ rental-store/src/main/java/com/pivovarit/domain/rental/RentalProdConfiguration.java
@@ -0,0 +1,9 @@
+package com.pivovarit.domain.rental;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("prod")
+class RentalProdConfiguration {
+}
