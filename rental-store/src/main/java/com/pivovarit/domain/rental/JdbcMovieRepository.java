a382259fa358fd1819d08dd883d7c050a0f68b2c
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-12-06 15:48:19 +0100

    6. JdbcClient persistence - solution

--- rental-store/src/main/java/com/pivovarit/domain/rental/JdbcMovieRepository.java
+++ rental-store/src/main/java/com/pivovarit/domain/rental/JdbcMovieRepository.java
@@ -1,5 +1,6 @@
 package com.pivovarit.domain.rental;
 
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Component;
 
@@ -8,36 +9,49 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
-class JdbcMovieRepository implements MovieRepository {
-
-    private final JdbcClient jdbcClient;
-
-    JdbcMovieRepository(JdbcClient jdbcClient) {
-        this.jdbcClient = jdbcClient;
-    }
+record JdbcMovieRepository(JdbcClient jdbcClient) implements MovieRepository {
 
     @Override
     public MovieId save(Movie movie) {
-        return null;
+        jdbcClient.sql("INSERT INTO movies(id, title, type) VALUES(?, ?, ?)")
+          .param(movie.id().id())
+          .param(movie.title())
+          .param(movie.type().toString())
+          .update();
+
+        return movie.id();
     }
 
     @Override
     public Collection<Movie> findAll() {
-        return null;
+        return jdbcClient.sql("SELECT * FROM movies").query(asMovie()).list();
     }
 
     @Override
     public Optional<Movie> findByTitle(String title) {
-        return Optional.empty();
+        return jdbcClient.sql("SELECT * FROM movies m WHERE m.title = ?")
+          .param(title)
+          .query(asMovie())
+          .optional();
     }
 
     @Override
     public List<Movie> findByType(MovieType type) {
-        return null;
+        return jdbcClient.sql("SELECT * FROM movies m WHERE m.type = ?")
+          .param(type.toString())
+          .query(asMovie())
+          .list();
     }
 
     @Override
     public Optional<Movie> findById(MovieId id) {
-        return Optional.empty();
+        return jdbcClient.sql("SELECT * FROM movies m WHERE m.id = ?")
+          .param(id.id())
+          .query(asMovie())
+          .optional();
+    }
+
+    private static RowMapper<Movie> asMovie() {
+        return (rs, __) -> new Movie(new MovieId(rs.getLong("id")), rs.getString("title"), MovieType.valueOf(rs.getString("type")));
     }
 }
