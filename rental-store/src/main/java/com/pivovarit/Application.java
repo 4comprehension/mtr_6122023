8658bf3c055d6a95c4be26163200d4b3b2d7ed4c
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-12-07 09:30:04 +0100

    8. Descriptions module integration - exercise

--- rental-store/src/main/java/com/pivovarit/Application.java
+++ rental-store/src/main/java/com/pivovarit/Application.java
@@ -3,6 +3,7 @@ package com.pivovarit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// TODO dodać 'description' do MovieDto i uzupełniać na podstawie danych z modułu 'descriptions'
 @SpringBootApplication
 public class Application {
 
