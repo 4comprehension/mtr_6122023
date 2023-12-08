0a11171123bae738cd3465612d75efa89c509fa4
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-12-08 11:09:19 +0100

    16. Concurrency control issues - ParallelRenter

---
+++ rental-store/src/main/java/com/pivovarit/utils/ParallelReturner.java
@@ -0,0 +1,40 @@
+package com.pivovarit.utils;
+
+import org.springframework.web.client.RestClient;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class ParallelReturner {
+
+    public static void main(String[] args) {
+        var client = RestClient.builder()
+          .baseUrl("http://localhost:8081")
+          .build();
+
+        int parallelism = 100;
+        try (var executor = Executors.newFixedThreadPool(parallelism)) {
+
+            CountDownLatch countDownLatch = new CountDownLatch(parallelism);
+
+            for (int i = 1; i <= parallelism; i++) {
+                int finalI = i;
+                executor.submit(() -> {
+                    countDownLatch.countDown();
+                    try {
+                        countDownLatch.await();
+                    } catch (InterruptedException e) {
+                    }
+                    client.post()
+                      .uri("/movies/{id}/rent", finalI)
+                      .body(new Account(1))
+                      .retrieve();
+                });
+            }
+        }
+    }
+
+    record Account(long accountId) {
+    }
+}
