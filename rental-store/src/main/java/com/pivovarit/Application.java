13beef5c4feaaaf7d9db2096467bb7725d9c183f
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-12-08 11:39:19 +0100

    16. Concurrency control issues - exercise

--- rental-store/src/main/java/com/pivovarit/Application.java
+++ rental-store/src/main/java/com/pivovarit/Application.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+    // TODO dodać kolumnę account_version BIGINT (unique constraint na account_version + account_id)
+    // TODO jak zmieniamy stan accounta, to zapisujemy event z podbitą wersją o 1
+    // TODO dodać ponawianie (3 razy) przy każdym wykryciu współbieżnej modyfikacji account
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
